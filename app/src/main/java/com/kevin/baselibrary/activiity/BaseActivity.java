60c4a3382aaf7b24c66df7ca872de76bcb984fa4
bailiangjin <bailiangjin@qq.com>
2015-09-29 21:17:47 +0800

    添加 Home 键 监听

--- app/src/main/java/com/kevin/baselibrary/activiity/BaseActivity.java
+++ app/src/main/java/com/kevin/baselibrary/activiity/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.kevin.baselibrary.activiity;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.kevin.baselibrary.listener.HomeListener;
+import com.kevin.baselibrary.utils.LogUtils;
 import com.kevin.baselibrary.utils.ToastUtils;
 
 /**
@@ -45,6 +48,57 @@ public class BaseActivity extends FragmentActivity {
      */
     protected void longShow(int resId) {
         ToastUtils.longShow(BaseActivity.this, resId);
+    }
+    protected HomeListener homeListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerHomeListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        homeListener.startWatch();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        homeListener.stopWatch();
+    }
+
+
+    /**
+     * 注册Home键的监听
+     */
+    protected void registerHomeListener() {
+        homeListener = new HomeListener(BaseActivity.this);
+        homeListener.setOnHomePressedListener(new HomeListener.OnHomePressedListener() {
+
+            @Override
+            public void onHomePressed() {
+                LogUtils.e("onHomePressed:::SuperBaseActivity");
+                BaseActivity.this.onHomePressed();
+            }
+
+            @Override
+            public void onHomeLongPressed() {
+                LogUtils.e("onHomeLongPressed:::SuperBaseActivity");
+                BaseActivity.this.onHomeLongPressed();
+            }
+        });
+        homeListener.startWatch();
+    }
+
+    protected  void onHomePressed(){
+
+    }
+
+    protected  void onHomeLongPressed(){
+
     }
 
 
