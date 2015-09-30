ba1ac97512fca1801780839c5d457630927838c1
bailiangjin <bailiangjin@qq.com>
2015-09-30 16:00:47 +0800

    完善 MyHttpUtils

--- app/src/main/java/com/kevin/baselibrary/utils/MyHttpUtils.java
+++ app/src/main/java/com/kevin/baselibrary/utils/MyHttpUtils.java
@@ -1,24 +1,100 @@
-//package com.kevin.baselibrary.utils;
-//
-//import com.kevin.baselibrary.callback.HttpCallback;
-//import com.squareup.okhttp.Callback;
-//import com.squareup.okhttp.FormEncodingBuilder;
-//import com.squareup.okhttp.OkHttpClient;
-//import com.squareup.okhttp.Request;
-//import com.squareup.okhttp.RequestBody;
-//import com.squareup.okhttp.Response;
-//
-//import java.io.IOException;
-//import java.util.Iterator;
-//import java.util.Map;
-//
-///**
-// * 作者  liangjin.bai Email:bailiangjin@gmail.com
-// * 创建时间 2015/8/25 14:40
-// */
-//
-//public class MyHttpUtils {
-//
+package com.kevin.baselibrary.utils;
+
+import com.kevin.baselibrary.callback.HttpCallback;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * 作者  liangjin.bai Email:bailiangjin@gmail.com
+ * 创建时间 2015/8/25 14:40
+ */
+
+public class MyHttpUtils {
+
+
+    public
+    static void asyncGet(final String url, final Map<String, Object> pairs, final HttpCallback httpCallback) {
+        LogUtils.e("localh2asyncPost:url=" + url);
+        String queryString = "";
+        for (Map.Entry<String, Object> entry : pairs.entrySet()) {
+            String key = entry.getKey();
+            String value = (String) entry.getValue();
+            queryString += key + "=" + value + "&";
+            LogUtils.e("h2asyncPost:" + key + "=" + value);
+        }
+        LogUtils.e("h2asyncGet:queryString=" + queryString);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url + "?" + queryString)
+                .build();
+        try {
+            client.newCall(request).enqueue(new Callback() {
+                public void onFailure(Request request_, IOException e) {
+                    httpCallback.onFailed(e.getMessage());
+                }
+
+                public void onResponse(Response response_) throws IOException {
+                    httpCallback.onSucess(response_.body().string());
+                }
+            });
+        } catch (Exception e) {
+            LogUtils.e("h2asyncGet:error=" + e.getMessage());
+            e.printStackTrace();
+
+            httpCallback.onFailed(e.getMessage());
+        }
+    }
+
+
+    public static void asyncPost(final String url, final Map<String, Object> pairs, final HttpCallback httpCallback) {
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LogUtils.e("localh2asyncPost:url=" + url);
+                OkHttpClient client = new OkHttpClient();
+                FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
+
+                for (Map.Entry<String, Object> entry : pairs.entrySet()) {
+                    formEncodingBuilder.add(entry.getKey(), (String) entry.getValue());
+                    LogUtils.e("h2asyncPost:" + entry.getKey() + "=" + (String) entry.getValue());
+                }
+                RequestBody formBody = formEncodingBuilder.build();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(formBody)
+                        .build();
+                try {
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+                            httpCallback.onFailed(e.getMessage());
+                        }
+
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+                            httpCallback.onSucess(response.body().string());
+                        }
+                    });
+                } catch (Exception e) {
+                    LogUtils.e("h2asyncPost:error=" + e.getMessage());
+                    e.printStackTrace();
+                    httpCallback.onFailed(e.getMessage());
+                }
+            }
+        }).start();
+
+    }
+
+
 //    public
 //    static String send(String url, String method, Map<String, Object> pairs, HttpCallback callBack) {
 //        if (method.equals("post"))
@@ -56,35 +132,6 @@
 //        return null;
 //    }
 //
-//    public
-//    static void asyncGet(String url, Map<String, Object> pairs, HttpCallback callBack) {
-//        println "h2asyncGet:url=$url;pairs=$pairs"
-//        String queryString = "";
-//        pairs.each {
-//            _m ->
-//                    queryString += _m.key + "=" + _m.value.toString() + "&"
-//        }
-//        println "h2asyncGet:queryString=$queryString"
-//        OkHttpClient client = new OkHttpClient();
-//        Request request = new Request.Builder()
-//                .url("$url?$queryString")
-//                .build();
-//        Response response = null;
-//        try {
-//            client.newCall(request).enqueue(new Callback() {
-//                public void onFailure(Request request_, IOException e) {
-//                    callBack.onFailed(e.getMessage());
-//                }
-//
-//                public void onResponse(Response response_) throws IOException {
-//                    callBack.onSucess(response_.body().string());
-//                }
-//            });
-//        } catch (IOException e) {
-//            println "h2asyncGet:error=" + e.getMessage()
-//            e.printStackTrace();
-//        }
-//    }
 //
 //    public static String post(String url, Map<String, Object> pairs) {
 //        println "h2post:url=$url;pairs=$pairs"
@@ -113,80 +160,5 @@
 //        }
 //        return null;
 //    }
-//
-//    public
-//    static asyncPost(final String url, final Map<String, Object> pairs, final HttpCallback callBack) {
-//        println "h2asyncPost:url=$url;pairs=$pairs"
-//        OkHttpClient client = new OkHttpClient();
-//        FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
-//        pairs.each {
-//            _m ->
-//                    formEncodingBuilder.add(_m.key, _m.value.toString());
-//            println "h2asyncPost:" + _m.key + "=" + _m.value.toString()
-//        }
-//        RequestBody formBody = formEncodingBuilder.build();
-//        Request request = new Request.Builder()
-//                .url(url)
-//                .post(formBody)
-//                .build();
-//        try {
-//            client.newCall(request).enqueue(new Callback() {
-//                public void onFailure(Request request_, IOException e) {
-//                    callBack.onFailed(e.getMessage());
-//                }
-//
-//                public void onResponse(Response response_) throws IOException {
-//                    callBack.onSucess(response_.body().string());
-//                }
-//            });
-//        } catch (IOException e) {
-//            LogUtils.e("h2asyncPost:error=" + e.getMessage());
-//            e.printStackTrace();
-//            callBack.onError(e.getMessage());
-//        }
-//    }
-//
-//    public static void asyncPost(final String url, final Map<String, Object> pairs, final HttpCallback httpCallback) {
-//
-//
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                LogUtils.e("localh2asyncPost:url=" + url);
-//                OkHttpClient client = new OkHttpClient();
-//                FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
-//
-//                Iterator<Map.Entry<String, Object>> iterator = pairs.entrySet().iterator();
-//                while (iterator.hasNext()) {
-//                    Map.Entry<String, Object> entry = iterator.next();
-//                    formEncodingBuilder.add(entry.getKey(), (String) entry.getValue());
-//                    LogUtils.e("h2asyncPost:" + entry.getKey() + "=" + (String) entry.getValue());
-//                }
-//                RequestBody formBody = formEncodingBuilder.build();
-//                Request request = new Request.Builder()
-//                        .url(url)
-//                        .post(formBody)
-//                        .build();
-//                try {
-//                    client.newCall(request).enqueue(new Callback() {
-//                        @Override
-//                        public void onFailure(Request request, IOException e) {
-//                            httpCallback.onFailed(e.getMessage());
-//                        }
-//
-//                        @Override
-//                        public void onResponse(Response response) throws IOException {
-//                            httpCallback.onSucess(response.body().string());
-//                        }
-//                    });
-//                } catch (Exception e) {
-//                    LogUtils.e("h2asyncPost:error=" + e.getMessage());
-//                    e.printStackTrace();
-//                    httpCallback.onFailed(e.getMessage());
-//                }
-//            }
-//        }).start();
-//
-//    }
-//
-//}
+
+}
