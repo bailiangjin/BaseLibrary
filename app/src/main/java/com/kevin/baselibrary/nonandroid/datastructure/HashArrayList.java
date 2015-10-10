2955dbe5f8c5d68e8739843cb4fc73cd7a226bb5
bailiangjin <bailiangjin@qq.com>
2015-10-10 18:34:14 +0800

    完善 自定义HanshList 逻辑 解决部分问题

--- app/src/main/java/com/kevin/baselibrary/nonandroid/datastructure/HashArrayList.java
+++ app/src/main/java/com/kevin/baselibrary/nonandroid/datastructure/HashArrayList.java
@@ -1,4 +1,4 @@
-package com.kevin.baselibrary.datastructure;
+package com.kevin.baselibrary.nonandroid.datastructure;
 
 import android.support.annotation.NonNull;
 
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * 自定义 哈希 list 保证list中无重复元素
@@ -30,21 +31,32 @@ public class HashArrayList extends ArrayList {
     }
 
     @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        if (fromIndex == toIndex) {
+            return;
+        }
+        for (int i = fromIndex; i < toIndex; i++) {
+            hashSet.remove(get(i));
+        }
+        super.removeRange(fromIndex, toIndex);
+    }
+
+    @Override
     public boolean addAll(Collection collection) {
-        Collection hashCollection = toHashCollection(collection);
-        hashSet.addAll(hashCollection);
-        return super.addAll(hashCollection);
+        Collection abHashCollection = toABHashCollection(collection);
+        hashSet.addAll(abHashCollection);
+        return super.addAll(abHashCollection);
     }
 
     @Override
     public boolean addAll(int index, Collection collection) {
-        Collection hashCollection = toHashCollection(collection);
-        hashSet.addAll(hashCollection);
-        return super.addAll(index, hashCollection);
+        Collection abHashCollection = toABHashCollection(collection);
+        hashSet.addAll(abHashCollection);
+        return super.addAll(index, abHashCollection);
     }
 
     /**
-     * 将 collection 转为不包含重复元素的HashCollection 标准 元素哈希值
+     * 将 collection 转为不包含自身原来重复元素的HashCollection 标准 元素哈希值
      *
      * @param collection
      * @return
@@ -56,6 +68,26 @@ public class HashArrayList extends ArrayList {
         }
         HashSet allSet = new HashSet(collection);
         Collection hashCollection = Arrays.asList(allSet.toArray());
+        return hashCollection;
+    }
+
+    /**
+     * 将 collection 转为不包含重复元素的HashCollection 并删除HashSet中已包含的元素
+     *
+     * @param collection
+     * @return
+     */
+    @NonNull
+    private Collection toABHashCollection(Collection collection) {
+        Collection hashCollection = toHashCollection(collection);
+        //遍历删除HashSet中已包含的元素
+        Iterator iterator = collection.iterator();
+        while(iterator.hasNext()){
+            Object obj = iterator.next();
+            if (hashSet.contains(obj)) {
+                iterator.remove();
+            }
+        }
         return hashCollection;
     }
 
