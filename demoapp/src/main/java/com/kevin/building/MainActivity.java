654b5f91699438f579dfa269dd0cc5e237787574
bailiangjin <bailiangjin@qq.com>
2015-10-12 11:35:05 +0800

    添加 demo 工程2

---
+++ demoapp/src/main/java/com/kevin/building/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kevin.building;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.kevin.building.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+
+}
---
+++ demoapp/src/main/java/com/kevin/building/base/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.kevin.building.base;
+
+import com.kevin.baselibrary.base.SuperBaseActivity;
+
+/**
+ * Author:  liangjin.bai
+ * Email: bailiangjin@gmail.com
+ * Create Time: 2015/10/12 10:40
+ */
+public class BaseActivity extends SuperBaseActivity{
+
+
+}
