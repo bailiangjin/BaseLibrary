bceee3b1a951f86e93cfacd7ad299e4c824eafee
bailiangjin <bailiangjin@qq.com>
2015-10-13 17:21:43 +0800

    添加 action

---
+++ baselibrary/src/main/java/com/kevin/baselibrary/constant/SuperBroadcastAction.java
@@ -0,0 +1,15 @@
+package com.kevin.baselibrary.constant;
+
+/**
+ * 广播 action 常量类
+ * Author:  liangjin.bai
+ * Email: bailiangjin@gmail.com
+ * Create Time: 2015/10/13 16:23
+ */
+public class SuperBroadcastAction {
+
+    /**
+     * 网络状态改变 intent filter
+     */
+    public static final String NETWORK_CHANGE_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
+}
---
+++ demoapp/src/main/java/com/kevin/building/constants/BroadcastAction.java
@@ -0,0 +1,19 @@
+package com.kevin.building.constants;
+
+import com.kevin.baselibrary.constant.SuperBroadcastAction;
+
+/**
+ * Author:  liangjin.bai
+ * Email: bailiangjin@gmail.com
+ * Create Time: 2015/10/13 16:34
+ */
+public class BroadcastAction extends SuperBroadcastAction {
+    /**
+     * 数据推送广播Action
+     */
+    public static final String PUSH_DATA_ACTION = "data.push.action";
+    /**
+     * 应用升级广播Action
+     */
+    public static final String NEW_VERSION_ACTION = "apk.update.action";
+}
