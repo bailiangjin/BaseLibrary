a7e213be9f38c174b88a9a374d0175d324739574
bailiangjin <bailiangjin@qq.com>
2015-10-13 19:07:45 +0800

    添加版本号添加逻辑

--- demoapp/src/main/java/com/kevin/building/activity/SplashActivity.java
+++ demoapp/src/main/java/com/kevin/building/activity/SplashActivity.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.TextView;
 
+import com.kevin.baselibrary.utils.AppUtil;
 import com.kevin.building.R;
 import com.kevin.building.base.BaseActivity;
 import com.kevin.building.utils.ActivityUtils;
 
 public class SplashActivity extends BaseActivity
 {
+
+	private TextView tv_app_version_name;
+
+	@Override
+	protected int getLayoutResId()
+	{
+		return R.layout.activity_splash;
+	}
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -28,25 +39,22 @@ public class SplashActivity extends BaseActivity
 				SplashActivity.this.finish();
 
 			}
-		}, 300);
+		}, 1000);
 
 	}
 
 	@Override
 	protected void initView() {
 
+		tv_app_version_name = (TextView) findViewById(R.id.tv_app_version_name);
 	}
 
 	@Override
 	protected void initLogic() {
-
+		tv_app_version_name.setText(AppUtil.getCurrentAppVersionName());
 	}
 
-	@Override
-	protected int getLayoutResId()
-	{
-		return R.layout.activity_splash;
-	}
+
 
 	@Override
 	public void onViewClick(View v)
