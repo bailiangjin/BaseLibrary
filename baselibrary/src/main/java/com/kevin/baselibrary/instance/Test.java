1326969a09598f0b89cb2d721ce6f9bdbc20b086
bailiangjin <bailiangjin@qq.com>
2015-10-29 18:41:43 +0800

    添加jni

---
+++ baselibrary/src/main/java/com/kevin/baselibrary/instance/Test.java
@@ -0,0 +1,9 @@
+package com.kevin.baselibrary.instance;
+
+/**
+ * Author:  liangjin.bai
+ * Email: bailiangjin@gmail.com
+ * Create Time: 2015/10/29 11:04
+ */
+public class Test {
+}
---
+++ baselibrary/src/main/java/com/kevin/baselibrary/instance/jni/NdkJniUtils.java
@@ -0,0 +1,14 @@
+package com.kevin.baselibrary.instance.jni;
+
+/**
+ * 作者：bailiangjin  bailiangjin@gmail.com
+ * 创建时间：15/10/28 22:31
+ */
+public class NdkJniUtils {
+
+    static {
+        System.loadLibrary("testjni");	//defaultConfig.ndk.moduleName
+    }
+
+    public native String getCLanguageString();
+}
