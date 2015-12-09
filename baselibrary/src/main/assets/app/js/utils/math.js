b55eb39192db8c148bbf697329e6c0180652df2e
bailiangjin <bailiangjin@qq.com>
2015-12-09 09:57:32 +0800

    规范js

--- baselibrary/src/main/assets/app/js/utils/math.js
+++ baselibrary/src/main/assets/app/js/utils/math.js
@@ -1,8 +1,8 @@
 // math.js
-define([ 'baseUtils' ],function($) {
+define([ 'baseUtils' ],function(baseUtils) {
 //define(function($) {
     var add = function(x, y) {
-        alert("内部调用baseUtils:function isEmpty:"+$.isEmpty("test"));
+        alert("内部调用baseUtils:function isEmpty:"+baseUtils.isEmpty("test"));
         return x + y;
     };
 
