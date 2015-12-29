0ba81f126a3547d1bcce1ef7f41f1e657d8a5cc1
bailiangjin <bailiangjin@qq.com>
2015-12-29 09:12:42 +0800

    添加 动态生成页面demo

---
+++ demoapp/src/main/java/com/kevin/building/ui/demo/dynamic/PageParamBean.java
@@ -0,0 +1,11 @@
+package com.kevin.building.ui.demo.dynamic;
+
+/**
+ * Author:  liangjin.bai
+ * Email: bailiangjin@gmail.com
+ * Create Time: 2015/12/28 14:23
+ */
+public class PageParamBean {
+
+
+}
