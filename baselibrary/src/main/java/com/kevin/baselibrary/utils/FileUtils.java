87661f24561b730b7f2286567bd7612d238694e4
bailiangjin <bailiangjin@qq.com>
2016-02-27 14:27:57 +0800

    添加 AssetUtils工具类

--- baselibrary/src/main/java/com/kevin/baselibrary/utils/FileUtils.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/utils/FileUtils.java
@@ -595,41 +595,7 @@ public class FileUtils {
         return (file.exists() && file.isFile() ? file.length() : -1);
     }
 
-    /**
-     * 从assets目录中复制整个文件夹内容
-     *
-     * @param context Context 使用CopyFiles类的Activity
-     * @param assetsFilePath String  原文件路径  如：assets/aa 传参为 aa
-     * @param desFilePath String  复制后路径  如：xx:/bb/cc
-     */
-    public static boolean copyFilesFassets(Context context, String assetsFilePath, String desFilePath) {
-        try {
-            String fileNames[] = context.getAssets().list(assetsFilePath);//获取assets目录下的所有文件及目录名
-            if (fileNames.length > 0) {//如果是目录
-                File file = new File(desFilePath);
-                file.mkdirs();//如果文件夹不存在，则递归
-                for (String fileName : fileNames) {
-                    copyFilesFassets(context, assetsFilePath + "/" + fileName, desFilePath + "/" + fileName);
-                }
-            } else {//如果是文件
-                InputStream is = context.getAssets().open(assetsFilePath);
-                FileOutputStream fos = new FileOutputStream(new File(desFilePath));
-                byte[] buffer = new byte[1024];
-                int byteCount = 0;
-                while ((byteCount = is.read(buffer)) != -1) {//循环从输入流读取 buffer字节
-                    fos.write(buffer, 0, byteCount);//将读取的输入流写入到输出流
-                }
-                fos.flush();//刷新缓冲区
-                is.close();
-                fos.close();
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+
 
     /* 遍历接收一个文件路径，然后把文件子目录中的所有文件遍历并输出来  */
     public static List<String>  getAllFiles(File root) {
