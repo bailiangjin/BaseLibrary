58db1f98c2e401e53ddafa4b4afe75268bb036ac
bailiangjin <bailiangjin@qq.com>
2016-03-02 23:58:26 +0800

    删除无用类

--- baselibrary/src/main/java/com/kevin/baselibrary/javase/Test.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/javase/Test.java
@@ -1,6 +1,5 @@
 package com.kevin.baselibrary.javase;
 
-import com.kevin.baselibrary.javase.datastructure.HashArrayList;
 
 /**
  * Author:  liangjin.bai
@@ -10,42 +9,5 @@ import com.kevin.baselibrary.javase.datastructure.HashArrayList;
 public class Test {
 
     public static void main(String[] args) {
-        HashArrayList list = new HashArrayList();
-        HashArrayList list2 = new HashArrayList();
-
-        list.add("test");
-        list.add("test");
-        list.add("test1");
-        list.add("test1");
-        list.add("test0");
-        list.add("test0");
-        list.add("test1");
-
-        list2.add("test1");
-        list2.add("test1");
-        list2.add("test3");
-        list2.add("test3");
-        list2.add("test5");
-        list2.add("test5");
-        list2.add("test6");
-
-        for (Object obj : list
-                ) {
-            System.out.print(" " + (String) obj + ":" + ((String) obj).hashCode());
-        }
-        System.out.println();
-
-        for (Object obj : list2
-                ) {
-            System.out.print(" " + (String) obj + ":" + ((String) obj).hashCode());
-        }
-        System.out.println();
-        list.addAll(0,list2);
-        list.remove("test");
-
-        for (Object obj : list
-                ) {
-            System.out.print(" "+(String) obj+":"+((String) obj).hashCode());
-        }
     }
 }
