bc7a164cf259b3c0f2505c718cae389f82ba0446
bailiangjin <bailiangjin@qq.com>
2016-05-31 00:04:38 +0800

    添加使用泛型的万能的 缓存类

--- baselibrary/src/main/java/com/kevin/baselibrary/javase/Test.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/javase/Test.java
@@ -1,5 +1,6 @@
 package com.kevin.baselibrary.javase;
 
+import com.kevin.baselibrary.tools.gcache.ThreeLevelCache;
 
 /**
  * Author:  liangjin.bai
@@ -9,5 +10,13 @@ package com.kevin.baselibrary.javase;
 public class Test {
 
     public static void main(String[] args) {
+        ThreeLevelCache<String> threeLevelCache= ThreeLevelCache.getInstance();
+        threeLevelCache.put("1","1");
+
+        ThreeLevelCache<Integer> threeLevelCache2= ThreeLevelCache.getInstance();
+        threeLevelCache2.put("1",2);
+
+        System.out.println(threeLevelCache.get("1",String.class));
+        System.out.println(threeLevelCache2.get("1",Integer.class));
     }
 }
