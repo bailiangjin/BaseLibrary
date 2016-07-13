51bec8a3a26bd61163dd73b92e766f28f54b71ff
bailiangjin <bailiangjin@qq.com>
2016-07-14 00:12:59 +0800

    添加 imageLoader demo

---
+++ ImageLoadDemo/src/main/java/com/bailiangjin/imageload/ImageLoadUtils.java
@@ -0,0 +1,33 @@
+package com.bailiangjin.imageload;
+
+/**
+ * Created by bailiangjin on 16/7/13.
+ */
+public class ImageLoadUtils {
+
+
+//    File cacheDir = StorageUtils.getCacheDirectory(context);
+//    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+//            .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
+//            .diskCacheExtraOptions(480, 800, null)
+//            .taskExecutor(...)
+//            .taskExecutorForCachedImages(...)
+//    .threadPoolSize(3) // default
+//    .threadPriority(Thread.NORM_PRIORITY - 2) // default
+//    .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+//    .denyCacheImageMultipleSizesInMemory()
+//    .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+//            .memoryCacheSize(2 * 1024 * 1024)
+//    .memoryCacheSizePercentage(13) // default
+//    .diskCache(new UnlimitedDiskCache(cacheDir)) // default
+//            .diskCacheSize(50 * 1024 * 1024)
+//    .diskCacheFileCount(100)
+//    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+//            .imageDownloader(new BaseImageDownloader(context)) // default
+//            .imageDecoder(new BaseImageDecoder()) // default
+//            .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+//            .writeDebugLogs()
+//    .build();
+
+
+}
