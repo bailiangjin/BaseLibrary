0cfe002f250e4a1aae2d850701ab78c176d72fa7
bailiangjin <bailiangjin@qq.com>
2016-07-17 00:27:09 +0800

    重构代码

--- ImageLoadDemo/src/main/java/com/bailiangjin/imageload/ImageLoadUtils.java
+++ ImageLoadDemo/src/main/java/com/bailiangjin/imageload/ImageLoadUtils.java
@@ -1,14 +1,20 @@
 package com.bailiangjin.imageload;
 
+import android.content.Context;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.display.BitmapDisplayer;
 import com.nostra13.universalimageloader.core.display.CircleBitmapDisplayer;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 import com.nostra13.universalimageloader.core.display.SimpleBitmapDisplayer;
 
 /**
+ * 基于Universal imageLoader 封装的 图片加载工具类
  * Created by bailiangjin on 16/7/16.
  */
 public enum ImageLoadUtils {
@@ -20,37 +26,32 @@ public enum ImageLoadUtils {
     private DisplayImageOptions circleOptions;
     private DisplayImageOptions roundedOptions;
 
-
     private ImageLoadUtils() {
 
-        normalOptions = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.drawable.icon_user)
-                .showImageForEmptyUri(R.drawable.icon_user)
-                .showImageOnFail(R.drawable.icon_user)
-                .cacheInMemory(true)
-                .cacheOnDisk(true)
-                .considerExifParams(true)
-                .displayer(new SimpleBitmapDisplayer())
-                .build();
+        int onLoadingImageResId=R.drawable.icon_user;
+        int onEmptyImageResId=R.drawable.icon_user;
+        int onFailedImageResId=R.drawable.icon_user;
 
-        circleOptions = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.drawable.icon_user)
-                .showImageForEmptyUri(R.drawable.icon_user)
-                .showImageOnFail(R.drawable.icon_user)
-                .cacheInMemory(true)
-                .cacheOnDisk(true)
-                .considerExifParams(true)
-                .displayer(new CircleBitmapDisplayer())
-                .build();
+        BitmapDisplayer simpleBitmapDisplayer =new SimpleBitmapDisplayer();
+        normalOptions =getOption(onLoadingImageResId,onEmptyImageResId, onFailedImageResId, simpleBitmapDisplayer);
 
-        roundedOptions = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.drawable.icon_user)
-                .showImageForEmptyUri(R.drawable.icon_user)
-                .showImageOnFail(R.drawable.icon_user)
+        BitmapDisplayer circleBitmapDisplayer =new CircleBitmapDisplayer();
+        circleOptions = getOption(onLoadingImageResId,onEmptyImageResId, onFailedImageResId, circleBitmapDisplayer);
+
+        BitmapDisplayer roundedBitmapDisplayer =new RoundedBitmapDisplayer(10);
+        roundedOptions = getOption(onLoadingImageResId,onEmptyImageResId, onFailedImageResId, roundedBitmapDisplayer);
+
+    }
+
+    private DisplayImageOptions getOption(int onLoadingImageResId,int onEmptyImageResId, int onFailedImageResId, BitmapDisplayer bitmapDisplayer) {
+        return new DisplayImageOptions.Builder()
+                .showImageOnLoading(onLoadingImageResId)
+                .showImageForEmptyUri(onEmptyImageResId)
+                .showImageOnFail(onFailedImageResId)
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
                 .considerExifParams(true)
-                .displayer(new RoundedBitmapDisplayer(10))
+                .displayer(bitmapDisplayer)
                 .build();
     }
 
@@ -68,5 +69,51 @@ public enum ImageLoadUtils {
     public void loadRoundedImageView(ImageView iv, String url) {
         ImageLoader.getInstance().displayImage(url, iv, roundedOptions);
     }
+
+
+
+    public  void init(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(80 * 1024 * 1024); // 80 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        config.writeDebugLogs(); // Remove for release app
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+    }
+
+
+
+
+    //    File cacheDir = StorageUtils.getCacheDirectory(context);
+//    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+//            .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
+//            .diskCacheExtraOptions(480, 800, null)
+//            .taskExecutor(...)
+//            .taskExecutorForCachedImages(...)
+//    .threadPoolSize(3) // default
+//    .threadPriority(Thread.NORM_PRIORITY - 2) // default
+//    .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+//    .denyCacheImageMultipleSizesInMemory()
+//    .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+//            .memoryCacheSize(2 * 1024 * 1024)
+//    .memoryCacheSizePercentage(13) // default
+//    .diskCache(new UnlimitedDiskCache(cacheDir)) // default
+//            .diskCacheSize(50 * 1024 * 1024)
+//    .diskCacheFileCount(100)
+//    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+//            .imageDownloader(new BaseImageDownloader(context)) // default
+//            .imageDecoder(new BaseImageDecoder()) // default
+//            .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+//            .writeDebugLogs()
+//    .build();
+
 
 }
--- ImageLoadDemo/src/main/java/com/bailiangjin/imageload/MyApplication.java
+++ ImageLoadDemo/src/main/java/com/bailiangjin/imageload/MyApplication.java
@@ -1,12 +1,6 @@
 package com.bailiangjin.imageload;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 /**
  * Created by bailiangjin on 16/7/16.
@@ -17,48 +11,11 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
-        initImageLoader(this);
-    }
-
-    public static void initImageLoader(Context context) {
-        // This configuration tuning is custom. You can tune every option, you may tune some of them,
-        // or you can create default configuration by
-        //  ImageLoaderConfiguration.createDefault(this);
-        // method.
-        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
-        config.threadPriority(Thread.NORM_PRIORITY - 2);
-        config.denyCacheImageMultipleSizesInMemory();
-        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
-        config.diskCacheSize(80 * 1024 * 1024); // 80 MiB
-        config.tasksProcessingOrder(QueueProcessingType.LIFO);
-        config.writeDebugLogs(); // Remove for release app
-
-        // Initialize ImageLoader with configuration.
-        ImageLoader.getInstance().init(config.build());
+        ImageLoadUtils.INSTANCE.init(this);
     }
 
 
-    //    File cacheDir = StorageUtils.getCacheDirectory(context);
-//    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
-//            .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
-//            .diskCacheExtraOptions(480, 800, null)
-//            .taskExecutor(...)
-//            .taskExecutorForCachedImages(...)
-//    .threadPoolSize(3) // default
-//    .threadPriority(Thread.NORM_PRIORITY - 2) // default
-//    .tasksProcessingOrder(QueueProcessingType.FIFO) // default
-//    .denyCacheImageMultipleSizesInMemory()
-//    .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
-//            .memoryCacheSize(2 * 1024 * 1024)
-//    .memoryCacheSizePercentage(13) // default
-//    .diskCache(new UnlimitedDiskCache(cacheDir)) // default
-//            .diskCacheSize(50 * 1024 * 1024)
-//    .diskCacheFileCount(100)
-//    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
-//            .imageDownloader(new BaseImageDownloader(context)) // default
-//            .imageDecoder(new BaseImageDecoder()) // default
-//            .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
-//            .writeDebugLogs()
-//    .build();
+
+
 
 }
