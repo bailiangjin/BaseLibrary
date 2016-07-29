2c94f789df303c248daca4803d6dca0f6c1d828c
bailiangjin <bailiangjin@qq.com>
2016-07-29 20:17:48 +0800

    添加 Activity 全局监听

--- baselibrary/src/main/java/com/kevin/baselibrary/app/AbsSuperApplication.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/app/AbsSuperApplication.java
@@ -1,7 +1,16 @@
 package com.kevin.baselibrary.app;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.kevin.baselibrary.utils.LogUtils;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by bailiangjin on 16/4/11.
@@ -11,11 +20,15 @@ public abstract class AbsSuperApplication extends Application {
     protected static Context context;
     protected static String appName;
 
+    private static List<Activity> mActivitys = Collections
+            .synchronizedList(new LinkedList<Activity>());
+
     @Override
     public void onCreate() {
         super.onCreate();
         context = this.getApplicationContext();
         appName =  getAppNameFromSub();
+        registerActivityListener();
     }
 
     public static String getAppName() {
@@ -27,5 +40,197 @@ public abstract class AbsSuperApplication extends Application {
     }
 
     protected abstract String  getAppNameFromSub();
+
+
+    /**
+     * @param activity 作用说明 ：添加一个activity到管理里
+     */
+    public void pushActivity(Activity activity) {
+        mActivitys.add(activity);
+        LogUtils.d("activityList:size:"+mActivitys.size());
+    }
+
+    /**
+     * @param activity 作用说明 ：删除一个activity在管理里
+     */
+    public void popActivity(Activity activity) {
+        mActivitys.remove(activity);
+        LogUtils.d("activityList:size:"+mActivitys.size());
+    }
+
+
+
+    /**
+     * get current Activity 获取当前Activity（栈中最后一个压入的）
+     */
+    public static Activity currentActivity() {
+        if (mActivitys == null||mActivitys.isEmpty()) {
+            return null;
+        }
+        Activity activity = mActivitys.get(mActivitys.size()-1);
+        return activity;
+    }
+
+    /**
+     * 结束当前Activity（栈中最后一个压入的）
+     */
+    public static void finishCurrentActivity() {
+        if (mActivitys == null||mActivitys.isEmpty()) {
+            return;
+        }
+        Activity activity = mActivitys.get(mActivitys.size()-1);
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public static void finishActivity(Activity activity) {
+        if (mActivitys == null||mActivitys.isEmpty()) {
+            return;
+        }
+        if (activity != null) {
+            mActivitys.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public static void finishActivity(Class<?> cls) {
+        if (mActivitys == null||mActivitys.isEmpty()) {
+            return;
+        }
+        for (Activity activity : mActivitys) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+            }
+        }
+    }
+
+    /**
+     * 按照指定类名找到activity
+     *
+     * @param cls
+     * @return
+     */
+    public static Activity findActivity(Class<?> cls) {
+        Activity targetActivity = null;
+        if (mActivitys != null) {
+            for (Activity activity : mActivitys) {
+                if (activity.getClass().equals(cls)) {
+                    targetActivity = activity;
+                    break;
+                }
+            }
+        }
+        return targetActivity;
+    }
+
+    /**
+     * @return 作用说明 ：获取当前最顶部activity的实例
+     */
+    public Activity getTopActivity() {
+        Activity mBaseActivity = null;
+        synchronized (mActivitys) {
+            final int size = mActivitys.size() - 1;
+            if (size < 0) {
+                return null;
+            }
+            mBaseActivity = mActivitys.get(size);
+        }
+        return mBaseActivity;
+
+    }
+
+    /**
+     * @return 作用说明 ：获取当前最顶部的acitivity 名字
+     */
+    public String getTopActivityName() {
+        Activity mBaseActivity = null;
+        synchronized (mActivitys) {
+            final int size = mActivitys.size() - 1;
+            if (size < 0) {
+                return null;
+            }
+            mBaseActivity = mActivitys.get(size);
+        }
+        return mBaseActivity.getClass().getName();
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public static void finishAllActivity() {
+        if (mActivitys == null) {
+            return;
+        }
+        for (Activity activity : mActivitys) {
+            activity.finish();
+        }
+        mActivitys.clear();
+    }
+
+    /**
+     * 退出应用程序
+     */
+    public  static void appExit() {
+        try {
+            LogUtils.e("app exit");
+            finishAllActivity();
+        } catch (Exception e) {
+        }
+    }
+
+
+    private void registerActivityListener() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+                @Override
+                public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                    //Activity 入栈
+                    pushActivity(activity);
+
+                }
+
+                @Override
+                public void onActivityStarted(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityResumed(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityPaused(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityStopped(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+                }
+
+                @Override
+                public void onActivityDestroyed(Activity activity) {
+                    if (null==mActivitys&&mActivitys.isEmpty()){
+                        return;
+                    }
+                    if (mActivitys.contains(activity)){
+                        popActivity(activity);
+                    }
+                }
+            });
+        }
+    }
 
 }
