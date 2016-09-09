57eadf8da80ee09add32f91c8baf80b6e35ad273
白良锦 <bailiangjin@bailiangjindeMacBook-Pro-2.local>
2016-09-09 11:12:00 +0800

    重构

--- demoapp/src/main/java/com/kevin/building/demo/dynamic/DetailActivity.java
+++ demoapp/src/main/java/com/kevin/building/demo/dynamic/DetailActivity.java
@@ -4,14 +4,13 @@ import android.os.Message;
 
 import com.kevin.building.R;
 import com.kevin.building.base.BaseActivity;
-import com.kevin.building.databinding.ActivityDynamicBinding;
 
 /**
  * Author:  liangjin.bai
  * Email: bailiangjin@gmail.com
  * Create Time: 2015/12/29 11:14
  */
-public class DetailActivity extends BaseActivity<ActivityDynamicBinding> {
+public class DetailActivity extends BaseActivity{
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_dynamic;
