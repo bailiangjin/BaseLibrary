e368bc5d020b901b90822b04ad9bed3324fc9486
白良锦 <bailiangjin@bailiangjindeMacBook-Pro-2.local>
2016-10-11 10:13:21 +0800

    完善ItemView

--- baselibrary/src/main/java/com/kevin/baselibrary/view/ItemView.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/view/ItemView.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -17,7 +18,7 @@ import com.kevin.baselibrary.R;
 /**
  * 标题栏 抽象类
  */
-public class ItemView extends FrameLayout {
+public abstract class ItemView extends FrameLayout {
 
     private RelativeLayout rl_root;
     private TextView tv_left;
@@ -35,7 +36,9 @@ public class ItemView extends FrameLayout {
         iv_left = (ImageView) findViewById(R.id.iv_left);
         iv_right = (ImageView) findViewById(R.id.iv_right);
         parseAttrs(context, attrs);
+
     }
+
 
     private void parseAttrs(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ItemView);
@@ -49,12 +52,14 @@ public class ItemView extends FrameLayout {
         setTextSize(tv_left, R.styleable.ItemView_left_text_size, typedArray);
         setTextSize(tv_right, R.styleable.ItemView_right_text_size, typedArray);
 
-        setViewVisibility(tv_left, R.styleable.ItemView_left_text_visibility, typedArray,true);
+        setViewSize(iv_left, R.styleable.ItemView_left_iv_width, R.styleable.ItemView_left_iv_height,typedArray);
+        setViewSize(iv_right, R.styleable.ItemView_right_iv_width, R.styleable.ItemView_right_iv_height,typedArray);
+
+        setViewVisibility(tv_left, R.styleable.ItemView_left_text_visibility, typedArray, true);
         setViewVisibility(tv_right, R.styleable.ItemView_right_text_visibility, typedArray);
 
-        setViewVisibility(iv_left, R.styleable.ItemView_left_iv_visibility, typedArray,true);
+        setViewVisibility(iv_left, R.styleable.ItemView_left_iv_visibility, typedArray, true);
         setViewVisibility(iv_right, R.styleable.ItemView_right_iv_visibility, typedArray);
-
 
         typedArray.recycle();
     }
@@ -64,7 +69,7 @@ public class ItemView extends FrameLayout {
         v.setVisibility(visibility ? VISIBLE : INVISIBLE);
     }
 
-    private void setViewVisibility(View v, int styleableId, TypedArray typedArray,boolean defaultVisibility) {
+    private void setViewVisibility(View v, int styleableId, TypedArray typedArray, boolean defaultVisibility) {
         boolean visibility = typedArray.getBoolean(styleableId, defaultVisibility);
         v.setVisibility(visibility ? VISIBLE : INVISIBLE);
     }
@@ -100,6 +105,16 @@ public class ItemView extends FrameLayout {
         }
     }
 
+    private void setViewSize(View view, int widthStyleableId, int heightStyleableId, TypedArray typedArray) {
+        int width = (int) typedArray.getDimension(widthStyleableId, -1f);
+        int height = (int) typedArray.getDimension(heightStyleableId, -1f);
+        if (width == -1 || height == -1) {
+            return;
+        }
+        setViewSize(view,width,height);
+
+    }
+
     private void setLeftText(String text) {
         if (TextUtils.isEmpty(text)) {
             return;
@@ -127,6 +142,25 @@ public class ItemView extends FrameLayout {
         }
         iv_right.setImageResource(resId);
     }
+
+
+    private void setViewSize(View view,int width, int height) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = width;
+        layoutParams.height = height;
+        view.invalidate();
+    }
+
+    private void setLeftIvSize(int width, int height) {
+        setViewSize(iv_left,width,height);
+    }
+
+    private void setRightIvSize(int width, int height) {
+        setViewSize(iv_right,width,height);
+    }
+
+
+
 
 
 }
---
+++ simpleim/src/main/java/com/bailiangjin/simpleim/view/MyItemView.java
@@ -0,0 +1,17 @@
+package com.bailiangjin.simpleim.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.kevin.baselibrary.view.ItemView;
+
+/**
+ * Created by bailiangjin on 16/10/11.
+ */
+
+public class MyItemView extends ItemView{
+    public MyItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
