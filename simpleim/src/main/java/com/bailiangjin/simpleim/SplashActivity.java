e3cd7a914672cc2538de84e328f7d6ea4dd8cf5f
白良锦 <bailiangjin@bailiangjindeMacBook-Pro-2.local>
2016-10-24 18:04:03 +0800

    commit

--- simpleim/src/main/java/com/bailiangjin/simpleim/SplashActivity.java
+++ simpleim/src/main/java/com/bailiangjin/simpleim/SplashActivity.java
@@ -12,63 +12,50 @@ import com.kevin.baselibrary.app.AppUtils;
 /**
  * Splash页
  */
-public class SplashActivity extends BaseActivity
-{
+public class SplashActivity extends BaseActivity {
 
-	private TextView tv_app_version_name;
+    private TextView tv_app_version_name;
 
-	@Override
-	protected int getLayoutResId()
-	{
-		return R.layout.activity_splash;
-	}
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_splash;
+    }
 
-	@Override
-	protected void initIntentData() {
+    @Override
+    protected void initIntentData() {
 
-	}
+    }
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		//跳转到 主页
-		new Handler().postDelayed(new Runnable()
-		{
-			@Override
-			public void run()
-			{
+        //跳转到 主页
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
-				if(AccountUtils.isLoginStatus()){
-					MainActivity.start(SplashActivity.this);
-				}else {
-					LoginActivity.start(SplashActivity.this);
-				}
-				SplashActivity.this.finish();
+                if (AccountUtils.isLoginStatus()) {
+                    MainActivity.start(SplashActivity.this);
+                } else {
+                    LoginActivity.start(SplashActivity.this);
+                }
+                SplashActivity.this.finish();
+            }
+        }, 1000);
 
+    }
 
+    @Override
+    protected void initView() {
+        hideCommonBaseTitle();
+        tv_app_version_name = (TextView) findViewById(R.id.tv_app_version_name);
+    }
 
-			}
-		}, 1000);
-
-	}
-
-	@Override
-	protected void initView() {
-		hideCommonBaseTitle();
-		tv_app_version_name = (TextView) findViewById(R.id.tv_app_version_name);
-	}
-
-	@Override
-	protected void initData() {
-		tv_app_version_name.setText(AppUtils.getCurrentAppVersionName());
-	}
-
-
-
-
-
+    @Override
+    protected void initData() {
+        tv_app_version_name.setText(AppUtils.getCurrentAppVersionName());
+    }
 
 }
