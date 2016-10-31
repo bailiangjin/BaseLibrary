a6a042d277e924c7b7735d7a86947f03044ea594
bailiangjin <bailiangjin@qq.com>
2016-10-31 23:13:46 +0800

    完善工具类

--- baselibrary/src/main/java/com/kevin/baselibrary/utils/device/NFCUtils.java
+++ baselibrary/src/main/java/com/kevin/baselibrary/utils/device/NFCUtils.java
@@ -5,6 +5,7 @@ import android.nfc.NfcManager;
 import android.provider.Settings;
 
 import com.kevin.baselibrary.app.AppUtils;
+import com.kevin.baselibrary.utils.LogUtils;
 
 /**
  * Created by bailiangjin on 16/9/23.
@@ -15,13 +16,22 @@ public class NFCUtils {
     public static boolean isNFCOpen() {
         NfcManager nfcManager = (NfcManager) AppUtils.getContext()
                 .getSystemService(Context.NFC_SERVICE);
-        return nfcManager.getDefaultAdapter().isEnabled();
+
+        return null != nfcManager && nfcManager.getDefaultAdapter().isEnabled();
     }
 
 
     public static void toSetNFC() {
-        String action= Settings.ACTION_NFC_SETTINGS;
-        DeviceSetUtils.toSetByAction(action);
+
+        NfcManager nfcManager = (NfcManager) AppUtils.getContext()
+                .getSystemService(Context.NFC_SERVICE);
+        if (null != nfcManager) {
+            String action = Settings.ACTION_NFC_SETTINGS;
+            DeviceSetUtils.toSetByAction(action);
+        } else {
+            LogUtils.e("当前设备不支持NFC");
+        }
+
     }
 
 }
